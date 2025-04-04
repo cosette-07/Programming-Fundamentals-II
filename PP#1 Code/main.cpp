5767428d00db89924791167ae7ac44b3e8e7f0f5
Cosette <cfehlhaber4@gmail.com>
2025-04-04 12:08:42 -0500

    Add files via upload

---
+++ PP#1 Code/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // Printing the tree first
+    std::cout << "   *   " << std::endl;
+    std::cout << "  ***  " << std::endl;
+    std::cout << " ***** " << std::endl;
+    std::cout << "*******" << std::endl;
+    std::cout << "  ***  " << std::endl;
+
+    // Creating two blank lines
+    std::cout << std::endl << std::endl;
+
+    // Printing the cat after
+    std::cout << "/\\   /\\" << std::endl;
+    std::cout << "  o o    " << std::endl;
+    std::cout << " =   =   " << std::endl;
+    std::cout << "  ---    " << std::endl;
+
+    return 0;
+}
